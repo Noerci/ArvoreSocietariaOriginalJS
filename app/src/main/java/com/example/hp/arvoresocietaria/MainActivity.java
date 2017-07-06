01fe53b13ad5e129b8e0e45c7384c467ec16a901
Noerci <noerci.junior@gmail.com>
2017-07-06 03:09:14 -0300

    Initial commit

---
+++ app/src/main/java/com/example/hp/arvoresocietaria/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.hp.arvoresocietaria;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String myurl="file:///android_asset/index.html";
+        WebView webView = (WebView) this.findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.loadUrl(myurl);
+
+    }
+}
